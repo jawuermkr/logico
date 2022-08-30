638b4ba73651b6bd00a6a9931b5fa8ef4d72fdc2
Nelson Jawuer Morales Santa <guitarro92@hotmail.com>
2022-08-30 16:49:54 -0500

    Rescate de código

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 <footer>
-		<hr>
-		<p>&copy 2020 | LÓGICO<br/>
-		info@verdaluno.com<br/>
-		<a href="http://verdaluno.com" target="_blank">verdaluno.com</a></p>
-	</footer>
+	<hr>
+	<p>&copy 2020 | LÓGICO<br/>
+	Tecnología social<br/>
+	info@verdaluno.com<br/>
+	<a href="http://verdaluno.com" target="_blank">verdaluno.com</a></p>
+</footer>
 
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
